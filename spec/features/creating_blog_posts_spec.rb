6c0b833e20915b398a3a5e74d7fb615c3ee47192
Kevin Leahey <kleahey@me.com>
2017-10-12 16:28:54 -0400

    LJ-4 edit a blog post and tests

--- spec/features/creating_blog_posts_spec.rb
+++ spec/features/creating_blog_posts_spec.rb
@@ -2,9 +2,13 @@ require 'simplecov'
 require 'rails_helper.rb'
 
 feature 'Creating blog posts' do
-  scenario 'can create a blog post.' do
+
+  background do
     visit '/blog'
     click_link 'New Post'
+  end
+
+  scenario 'can create a blog post.' do
     fill_in 'Title', with: 'My first blog post'
     fill_in 'Body', with: 'This is the body of my first blog post.'
     click_button 'Create Post'
@@ -14,9 +18,8 @@ feature 'Creating blog posts' do
   end
 
   it 'needs a title to create a post.' do
-    visit '/blog'
-    click_link 'New Post'
     click_button 'Create Post'
     expect(page).to have_content 'Your post must include a title!'
   end
+  
 end
